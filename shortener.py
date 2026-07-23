6082a9c8831ce17253c972855a71f9b3b7576791
labib87691-hue <labib87691@gmail.com>
2026-07-23 23:32:14 +0600

    Add files via upload

--- shortener.py
+++ shortener.py
@@ -5,33 +5,32 @@ import requests
 BANNER = r"""
   _        _    ____ ___ ____   _____ _____ ____  _    _   
  | |      / \  | __ )_ _| __ ) |_   _| ____/ ___|| |  | |
- | |     / _ \ |  _ \| |  _ \    | | |  _| | |   | |__| | 
- | |___ / ___ \| |_) | | |_) |   | | | |___| |___|  __  |
+ | |     / _ \ |  _ \| || _  \   | | |  _| | |   | |__| | 
+ | |___ / ___ \| |_) | |||_)  |  | | | |___| |___|  __  |
  |_____/_/   \_\____/___|____/   |_| |_____|\____| |  | |  
 ===========================================================
           terminal-url-shortener-labib v1.0
 ===========================================================
 """
 
-def shorten_url(long_url):
-    api_url = f"http://tinyurl.com/api-create.php?url={long_url}"
+def shorten_url_cleanuri(long_url):
+    api_url = "https://cleanuri.com/api/v1/shorten"
     try:
-        response = requests.get(api_url)
+        response = requests.post(api_url, data={'url': long_url})
         if response.status_code == 200:
-            return response.text
+            return response.json().get("result_url")
         else:
             return "Error: Link shorten করা সম্ভব হয়নি।"
     except Exception as e:
         return f"Error: {e}"
 
 if __name__ == "__main__":
-    # ডিজাইন প্রপ্রিন্ট করা
     print(BANNER)
     
     if len(sys.argv) > 1:
         target_url = sys.argv[1]
         print(f"Original URL  : {target_url}")
-        print(f"Shortened URL : {shorten_url(target_url)}\n")
+        print(f"Shortened URL : {shorten_url_cleanuri(target_url)}\n")
     else:
         print("Usage: python shortener.py <YOUR_URL>")
         print("Example: python shortener.py https://example.com\n")
